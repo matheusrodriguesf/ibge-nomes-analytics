0633557390a1fa8babf4872159b570c4c6f52223
Matheus Rodrigues <matheus.arcelino@mds.gov.br>
2026-04-24 13:19:58 -0300

    Importacao Projeto Base

---
+++ nome-br-api/src/main/java/br/com/arcelino/nomebr/NomeBrApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcelino.nomebr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomeBrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomeBrApiApplication.class, args);
+	}
+
+}

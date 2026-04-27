7d4cbffa7e2fa297f6eaad4574cc0f6d9cbfc8b3
Matheus Rodrigues <matheus.arcelino@mds.gov.br>
2026-04-26 23:12:26 -0300

    Traducao componente paginacao

--- nome-br-web/src/app/app.config.ts
+++ nome-br-web/src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 
 import { routes } from './app.routes';
+import { getPtBrPaginatorIntl } from './shared/i18n/mat-paginator-intl-pt-br.';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: MatPaginatorIntl, useFactory: getPtBrPaginatorIntl },
   ]
 };
